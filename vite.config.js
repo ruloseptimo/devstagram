d0993a651672b1e8a089c42ed271ffd277997ef4
Cristian Rojas <cristianrojas@MBP-de-Cristian.fibertel.com.ar>
2024-06-26 20:13:49 -0300

    importando Estilos vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js,'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: resolve(__dirname, 'resources/js/app.js'),
+            },
+        },
+    },
 });
